9d5b8301ff49bf8ed6501eb090d85fc6e3b85f81
ernesto2011 <skillmx107@gmail.com>
2025-04-15 22:07:41 -0600

    ..

--- app/routes.ts
+++ app/routes.ts
@@ -1,10 +1,17 @@
-import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  prefix,
+  route,
+} from "@react-router/dev/routes";
 
 export default [
-    index("routes/home.tsx"),
-
-    layout('layouts/auth-layout.tsx', [
-        route('login',"routes/auth/login-page.tsx"),
-        route('register', "routes/auth/register-page.tsx")
-    ])
+  index("routes/home.tsx"),
+  ...prefix("/auth", [
+    layout("layouts/auth-layout.tsx", [
+      route("login", "routes/auth/login-page.tsx"),
+      route("register", "routes/auth/register-page.tsx"),
+    ]),
+  ]),
 ] satisfies RouteConfig;
