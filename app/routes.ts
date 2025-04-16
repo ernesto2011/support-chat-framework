5c205cca3eb97b14db0c5c231a7594d001989c29
ernesto2011 <skillmx107@gmail.com>
2025-04-15 22:02:04 -0600

    :rocket: empezando proyecto con react-router como framework

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,10 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"),
+
+    layout('layouts/auth-layout.tsx', [
+        route('login',"routes/auth/login-page.tsx"),
+        route('register', "routes/auth/register-page.tsx")
+    ])
+] satisfies RouteConfig;
